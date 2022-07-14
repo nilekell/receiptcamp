04a69b9f59d07e0c0d3d194f47ae84c63ac5ed48
NiLo-1 <90138096+NiLo-1@users.noreply.github.com>
2022-07-14 16:40:37 +0100

    instantiated login page

---
+++ login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:receiptcamp/route_generator.dart';
+
+import 'route_generator.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // container to be replaced with actual login page
+    return Container(
+        child: Placeholder(
+      fallbackHeight: 400,
+      fallbackWidth: 400,
+    ));
+  }
+}
