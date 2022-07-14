8b08420a595611b20564e4538e6ec50332bdfa63
NiLo-1 <90138096+NiLo-1@users.noreply.github.com>
2022-07-14 16:44:10 +0100

    instantiated recently-viewed-receipts widget

---
+++ lib/widgets/home/recentlyviewed.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/* class RecentlyViewedReceipts extends StatefulWidget {
+  @override
+  State<RecentlyViewedReceipts> createState() => _RecentlyViewedReceipts();
+}
+
+class _RecentlyViewedReceipts extends State<_RecentlyViewedReceipts> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("placemarker");
+  }
+}
+*/
+
+/* ListView(
+          padding: const EdgeInsets.all(300),
+          children: <Widget>[
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+          ],
+        ) 
+*/
