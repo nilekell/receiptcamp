d3ac55ada38720a2afb8d0b3868ae5fa0d637b82
NiLo-1 <90138096+NiLo-1@users.noreply.github.com>
2022-07-15 11:24:54 +0100

    small

--- pages/login_page.dart
+++ pages/login_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:receiptcamp/pages/route_generator.dart';
-
-import 'route_generator.dart';
 
 class LoginPage extends StatelessWidget {
   @override
