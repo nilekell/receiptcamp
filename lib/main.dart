d86fadb518724583b7e65b38c565ea4a29685a05
Nile Kelly <nilekell@gmail.com>
2023-05-24 16:02:29 +0100

    implemented authentication service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:receiptcamp/logic/blocs/auth/authentication_bloc.dart';
+import 'package:receiptcamp/presentation/router/app_router.dart';
 import 'package:receiptcamp/presentation/screens/home.dart';
+import 'package:receiptcamp/presentation/screens/login.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(BlocProvider<AuthenticationBloc>(
+      create: (BuildContext context) => AuthenticationBloc()..add(AuthenticationInitialEvent()),
+      child: const MyApp(),
+    ));
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,11 +22,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ReceiptCamp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Home(),
+        debugShowCheckedModeBanner: false,
+        home: const BlocNavigate(),
+        onGenerateRoute: AppRouter().onGenerateRoute,
+        title: 'ReceiptCamp',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ));
+  }
+}
+
+class BlocNavigate extends StatefulWidget {
+  const BlocNavigate({Key? key}) : super(key: key);
+
+  @override
+  State<BlocNavigate> createState() => _BlocNavigateState();
+}
+
+class _BlocNavigateState extends State<BlocNavigate> {
+  late AuthenticationBloc authenticationBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    authenticationBloc = BlocProvider.of<AuthenticationBloc>(context);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthenticationBloc, AuthenticationState>(
+      bloc: authenticationBloc,
+      listener: (context, state) {},
+      builder: (context, state) {
+        print(state.runtimeType);
+        if (state is AuthenticationSuccessState) {
+          return const Home();
+        } else {
+          return const Login();
+        }
+      },
     );
   }
 }
---
+++ lib/models/app_user.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// class to abstract away unnecessary details from firebase User class
+// https://pub.dev/documentation/firebase_auth/latest/firebase_auth/User-class.html
+
+class AppUser {
+  final String? uid;
+  final String? email;
+
+  AppUser({
+    required this.uid,
+    required this.email,
+  });
+}
