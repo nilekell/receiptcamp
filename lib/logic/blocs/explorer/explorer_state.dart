91dd424e6ccd47f6e3102064159c51663d897822
Nile Kelly <nilekell@gmail.com>
2023-07-04 15:40:31 +0100

    passing current folder alongside its subfiles in ExplorerLoadedSuccessState() for use in explorer ui

--- lib/logic/blocs/explorer/explorer_state.dart
+++ lib/logic/blocs/explorer/explorer_state.dart
@@ -16,9 +16,10 @@ final class ExplorerLoadingState extends ExplorerState {}
 final class ExplorerEmptyFilesState extends ExplorerState {}
 
 final class ExplorerLoadedSuccessState extends ExplorerState {
-  const ExplorerLoadedSuccessState({required this.files});
+  const ExplorerLoadedSuccessState({required this.folder, required this.files});
 
   final List<dynamic> files;
+  final Folder folder;
 
   @override
   List<Object> get props => [files];
