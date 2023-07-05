95d1aca5b4d3a3d3485a6d6e4ab6aa22e3542c78
Nile Kelly <nilekell@gmail.com>
2023-07-05 10:20:03 +0100

    broken file explorer test

--- lib/presentation/screens/a_file_explorer.dart
+++ lib/presentation/screens/a_file_explorer.dart
@@ -8,6 +8,7 @@ import 'package:receiptcamp/models/receipt.dart';
 import 'package:receiptcamp/models/folder.dart';
 import 'package:receiptcamp/presentation/ui/file_explorer/folder/folder_sheet.dart';
 import 'package:receiptcamp/presentation/ui/file_explorer/receipt/receipt_sheet.dart';
+import 'package:receiptcamp/presentation/ui/file_explorer/snackbars/snackbar_utility.dart';
 import 'package:receiptcamp/presentation/ui/file_explorer/upload_sheet.dart';
 
 class FileExplorer extends StatefulWidget {
@@ -18,42 +19,67 @@ class FileExplorer extends StatefulWidget {
 }
 
 class _FileExplorerState extends State<FileExplorer> {
-  List<int> items = List.generate(16, (i) => i);
+  @override
+  void initState() {
+    super.initState();
+    context.read<ExplorerBloc>().add(ExplorerInitialEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider<UploadBloc>(
-            create: (context) => UploadBloc()..add(UploadInitialEvent()),
-          ),
-          BlocProvider<ExplorerBloc>(
-            create: (context) => ExplorerBloc()..add(ExplorerFetchFilesEvent(folderId: 'a1')),
-          ),
-          BlocProvider<FileEditingCubit>(
-            create: (context) => FileEditingCubit(),
-          ),
+          create: (context) => UploadBloc()..add(UploadInitialEvent()),
+        ),
+        BlocProvider<ExplorerBloc>(
+          create: (context) =>
+              ExplorerBloc()..add(ExplorerFetchFilesEvent(folderId: 'a1')),
+        ),
+        BlocProvider<FileEditingCubit>(
+          create: (context) => FileEditingCubit(),
+        ),
       ],
-      child: ExplorerView(items: items),
-          // - FolderName
-          // - BackButton
-          // - RefreshableFolderView
-          //     - RefreshIndicator
-          //     - ListView
-          //         - ReceiptTile
-          //         - FolderTile
-          //     - UploadButton
+      child: BlocBuilder<ExplorerBloc, ExplorerState>(
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case ExplorerLoadedSuccessState:
+              ExplorerLoadedSuccessState currentState =
+                  state as ExplorerLoadedSuccessState;
+              return ExplorerView(
+                items: currentState.files,
+                folder: currentState.folder,
+                previousFolderId: currentState.folder.parentId,
+              );
+            default:
+              print(state.runtimeType.toString());
+              return CircularProgressIndicator();
+          }
+        },
+      ),
+      // ExplorerView
+      // - FolderName
+      // - BackButton
+      // - RefreshableFolderView
+      //     - RefreshIndicator
+      //     - ListView
+      //         - ReceiptTile
+      //         - FolderTile
+      //     - UploadButton
     );
   }
 }
 
 class ExplorerView extends StatelessWidget {
-  const ExplorerView({
-    super.key,
-    required this.items,
-  });
+  const ExplorerView(
+      {super.key,
+      required this.items,
+      required this.folder,
+      required this.previousFolderId});
 
-  final List<int> items;
+  final List<dynamic> items;
+  final Folder folder;
+  final String previousFolderId;
 
   @override
   Widget build(BuildContext context) {
@@ -61,32 +87,45 @@ class ExplorerView extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          FolderName(),
+          folder.id != 'a1'
+              ? FolderName(
+                  name: folder.name,
+                )
+              : FolderName(
+                  name: 'All Receipts',
+                ),
           const SizedBox(
             height: 5.0,
           ),
-          BackButton(),
+          folder.id != 'a1'
+              ? BackButton(
+                  previousFolderId: previousFolderId,
+                  currentFolderId: folder.id,
+                )
+              : Container(),
           Expanded(
-            child: RefreshableFolderView(items: items),
+            child: RefreshableFolderView(folderId: folder.id, items: items),
           )
         ],
       ),
-      floatingActionButton: UploadButton(),
+      floatingActionButton: UploadButton(currentFolder: folder),
     );
   }
 }
 
 class FolderName extends StatelessWidget {
+  final String name;
   const FolderName({
     super.key,
+    required this.name,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: const Text(
-        'Folder Name',
+      child: Text(
+        name,
         style: TextStyle(
           fontSize: 34.0,
           fontWeight: FontWeight.bold,
@@ -97,109 +136,157 @@ class FolderName extends StatelessWidget {
 }
 
 class BackButton extends StatelessWidget {
-  const BackButton({
-    super.key,
-  });
+  final String previousFolderId;
+  final String currentFolderId;
+
+  const BackButton(
+      {super.key,
+      required this.previousFolderId,
+      required this.currentFolderId});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {}, 
-      icon: Icon(Icons.arrow_back));
+        onPressed: () {
+          context.read<ExplorerBloc>().add(ExplorerGoBackEvent(
+              currentFolderId: currentFolderId,
+              previousFolderId: previousFolderId));
+        },
+        icon: Icon(Icons.arrow_back));
   }
 }
 
 class RefreshableFolderView extends StatelessWidget {
-  const RefreshableFolderView({
-    super.key,
-    required this.items,
-  });
+  const RefreshableFolderView(
+      {super.key, required this.items, required this.folderId});
 
-  final List<int> items;
+  final List<dynamic> items;
+  final String folderId;
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-        onRefresh: () async {
-          return Future.delayed(Duration(seconds: 1));
-        },
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return FolderListTile(); // or ReceiptListTile()
+    return BlocConsumer<UploadBloc, UploadState>(
+      listener: (context, state) {
+        SnackBarUtility.showUploadSnackBar(context, state);
+      },
+      builder: (context, state) {
+        return BlocBuilder<UploadBloc, UploadState>(
+          builder: (context, state) {
+            return RefreshIndicator(
+                onRefresh: () async {
+                  // figure out how to only rebuild the listview with a refresh
+                  context
+                      .read<ExplorerBloc>()
+                      .add(ExplorerFetchFilesEvent(folderId: folderId));
+                },
+                child: items.isNotEmpty
+                    ? ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          var item = items[index];
+                          if (item is Receipt) {
+                            return ReceiptListTile(receipt: item);
+                          } else if (item is Folder) {
+                            return FolderListTile(folder: item);
+                          }
+                          return null;
+                        },
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.all(100.0),
+                        child: Text('No receipts/folders to show'),
+                      ));
           },
-        ));
+        );
+      },
+    );
   }
 }
 
 class FolderListTile extends StatelessWidget {
-  const FolderListTile({
-    super.key,
-  });
+  final Folder folder;
+
+  const FolderListTile({super.key, required this.folder});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-            leading: const Icon(Icons.folder),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.more,
-                size: 20.0,
-                color: Colors.brown[900],
+    return BlocConsumer<FileEditingCubit, FileEditingCubitState>(
+      listener: (context, state) {
+        SnackBarUtility.showFileEditSnackBar(context, state);
+      },
+      builder: (context, state) {
+        return ListTile(
+              leading: const Icon(Icons.folder),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.more,
+                  size: 20.0,
+                  color: Colors.brown[900],
+                ),
+                onPressed: () {
+                  showFolderOptions(
+                      context, context.read<FileEditingCubit>(), folder);
+                },
               ),
-              onPressed: () {
-                // showFolderOptions(
-                //     context,
-                //     context
-                //         .read<FileEditingCubit>(),
-                //     folder);
+              onTap: () {
+                context.read<ExplorerBloc>().add(ExplorerChangeFolderEvent(
+                    currentFolderId: folder.parentId, nextFolderId: folder.id));
               },
-            ),
-            title: Text('Folder name'), // folder.name
-            // can return some properties specific to Folder
-          );
+              title: Text(folder.name),
+            );
+          },
+        );
   }
 }
 
 class ReceiptListTile extends StatelessWidget {
-  const ReceiptListTile({
-    super.key,
-  });
+  final Receipt receipt;
+
+  const ReceiptListTile({super.key, required this.receipt});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-            leading: const Icon(Icons.receipt),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.more,
-                size: 20.0,
-                color: Colors.brown[900],
-              ),
-              onPressed: () {
-                // showReceiptOptions(
-                //     context,
-                //     context
-                //         .read<FileEditingCubit>(),
-                //     receipt);
-              },
-            ),
-            title: Text('Receipt name w/o file ending') // receipt.name.split('.').first
-            // can return some properties specific to Receipt
-          );
+    return BlocConsumer<FileEditingCubit, FileEditingCubitState>(
+      listener: (context, state) {
+        SnackBarUtility.showFileEditSnackBar(context, state);
+      },
+      builder: (context, state) {
+        return BlocBuilder<FileEditingCubit, FileEditingCubitState>(
+          builder: (context, state) {
+            return ListTile(
+                leading: const Icon(Icons.receipt),
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.more,
+                    size: 20.0,
+                    color: Colors.brown[900],
+                  ),
+                  onPressed: () {
+                    showReceiptOptions(
+                        context, context.read<FileEditingCubit>(), receipt);
+                  },
+                ),
+                onTap: () {
+                  // show receipt preview
+                },
+                title: Text(receipt.name.split('.').first));
+          },
+        );
+      },
+    );
   }
 }
 
 class UploadButton extends StatelessWidget {
-  const UploadButton({
-    super.key,
-  });
+  final Folder currentFolder;
+
+  const UploadButton({super.key, required this.currentFolder});
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () {
-        showUploadOptions(context, context.read<UploadBloc>());
+        showUploadOptions(context, context.read<UploadBloc>(), currentFolder);
       },
       child: const Icon(Icons.add),
     );
